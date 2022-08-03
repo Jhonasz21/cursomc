d68b316114ae30e1f121c1801ca97347d860a122
Jonas Cunha <jhonasz@hotmail.com>
2022-08-02 23:09:20 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/CategoriaRepository.java
@@ -1,7 +1,5 @@
 package com.nelioalves.cursomc.repositories;
 
-import java.io.Serializable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
