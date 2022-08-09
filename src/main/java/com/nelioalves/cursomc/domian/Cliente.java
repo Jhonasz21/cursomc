4ad56b13c611456261e19f58db3a8fef0394b815
Jonas Cunha <jhonasz@hotmail.com>
2022-08-09 20:42:14 -0300

    Endpoint cliente, id e disponivel

--- src/main/java/com/nelioalves/cursomc/domian/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domian/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.nelioalves.cursomc.domian.enums.TipoCliente;
 
 @Entity
@@ -29,8 +30,9 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
-	private List<Endereco> enderecos = new ArrayList();
+	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
